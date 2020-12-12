28378c59f19c84e0a8eb9941c4451f890ae4eb5c
17-sss <jhsonking@naver.com>
2020-12-12 16:52:49 +0900

    문자열 입력 값을 배열로 변환하는 arrActionCreate() 생성 & 알림 문구 추가

--- rubik.js
+++ rubik.js
@@ -1,36 +1,75 @@
 // [STEP-3]
 
-// cubeStateView, 현재 큐브 상태 반환 (String)
+// [function] START ****************************
+
+// 1. cubeStateView, 현재 큐브 상태 반환 (String)
 const cubeStateView = (aCube) => {
     let strResult = ``;
 
     // left front right back
     for (const key in aCube) {
-        if (key === "front" || key === "right" || key === "back") 
-            continue;
+        if (key === 'front' || key === 'right' || key === 'back') continue;
 
-        const arrProperty = aCube[key];        
+        const arrProperty = aCube[key];
 
-        for (let i=0; i < arrProperty.length; i++) {
+        for (let i = 0; i < arrProperty.length; i++) {
             const arrItem = arrProperty[i];
-            const strSplit = arrItem.join(' ');            
-            
+            const strSplit = arrItem.join(' ');
+
             let strTmp = '';
             if (key === 'left') {
-                strTmp = strSplit + '\t' 
-                    + aCube["front"][i].join(' ') + '\t' 
-                    + aCube["right"][i].join(' ') + '\t' 
-                    + aCube["back"][i].join(' ');                
+                strTmp =
+                    strSplit +
+                    '\t' +
+                    aCube['front'][i].join(' ') +
+                    '\t' +
+                    aCube['right'][i].join(' ') +
+                    '\t' +
+                    aCube['back'][i].join(' ');
             } else {
-                strTmp = '\t'+strSplit;                            
-            }                            
-            strResult += (i === arrProperty.length-1) ? strTmp + '\n\n' : strTmp + '\n';                    
+                strTmp = '\t' + strSplit;
+            }
+            strResult +=
+                i === arrProperty.length - 1
+                    ? key === 'down'
+                        ? strTmp
+                        : strTmp + '\n\n'
+                    : strTmp + '\n';
         }
     }
-    return '\n' + strResult;    
-}
+    return '\n' + strResult + '\n';
+};
 
-// readInput, 큐브 실행
+
+// 2. arrActionCreate, 입력 값을 배열로 변환.
+const arrActionCreate = (strAction) => {
+    const arrResult = [];
+
+    const arrAllow = ['U', 'L', 'F', 'R', 'B', 'D'];    
+    const arrActionTmp = strAction.split('');
+    
+    for (let i = 0; i < arrActionTmp.length; i++) {        
+        const strCheck = arrActionTmp[i].toUpperCase();
+        if (arrAllow.indexOf(strCheck) <= -1) continue;        
+
+        let strTmp = '';
+        if (arrActionTmp[i + 1] === "\'") {
+            strTmp = arrActionTmp[i + 2] === "2" 
+                ? arrActionTmp[i] + arrActionTmp[i + 1] + arrActionTmp[i + 2]
+                : arrActionTmp[i] + arrActionTmp[i + 1];           
+        } else {
+            strTmp = arrActionTmp[i + 1] === "2" 
+                ? arrActionTmp[i] + arrActionTmp[i + 1]
+                : arrActionTmp[i];
+        }
+        
+        arrResult.push(strTmp.toUpperCase());
+    }
+    
+    return arrResult;
+} 
+
+// 3. readInput, 큐브 실행
 const readInput = (aCube) => {
     const readline = require('readline');
     const rl = readline.createInterface({
@@ -38,13 +77,14 @@ const readInput = (aCube) => {
         output: process.stdout,
     });
 
-    console.log(cubeStateView(aCube));
+    console.log('\n-- 초기 상태 --\n' + cubeStateView(aCube));
 
-    rl.setPrompt("CUBE >");
+    rl.setPrompt('CUBE >');
     rl.prompt();
     rl.on('line', (strLine) => {
         if (strLine.toUpperCase() === 'Q') rl.close();
-        console.log(cubeStateView(aCube));
+        console.log(arrActionCreate(strLine));
+
         rl.prompt();
     }).on('close', () => {
         console.log('Bye~');
@@ -52,7 +92,12 @@ const readInput = (aCube) => {
     });
 };
 
-// 큐브 생성
+// [function] END ****************************
+
+
+// [Execute] START **************************** 
+
+// 1. 큐브 기본 값
 const cube = {
     up: Array.from({ length: 3 }, () => new Array(3).fill('B')),
     left: Array.from({ length: 3 }, () => new Array(3).fill('W')),
@@ -62,4 +107,20 @@ const cube = {
     down: Array.from({ length: 3 }, () => new Array(3).fill('R')),
 };
 
+// 2. 실행
+console.log(
+    '\n======================= [STEP-3] Rubik\'s Cube =======================' + '\n'
+    + '[info] 루빅스 큐브 게임에 오신걸 환영합니다. 동작을 입력해주세요.' + '\n'
+    + '   ' + '<입력 가능 값>' + '\n'
+    + '\t' + '1. 시계 방향 회전:   U L F R B D' + '\n'
+    + '\t' + '2. 시계 반대 방향 회전:   U\' L\' F\' R\' B\' D\'' + '\n'
+    + '\t' + '(!) 모든 값에는 숫자 2가 붙을 수 있습니다.' + '\n'
+    + '\t' + '    - 2가 뒤에 붙을 시 180도 회전, 평상시 90도 회전' + '\n'
+    + '   ' + '<입력 예>' + '\n'     
+    + '\t' + 'U  LB\'  F2D\'2  FRR\'U2R\'' + '\n\n'
+    + '[info] 종료하시려면 \'알파벳 Q(q)\'를 입력하거나 Ctrl + C를 눌러주세요.' + '\n'
+    + '====================================================================='
+);  
 readInput(cube);
+
+// [Execute]  START **************************** 
